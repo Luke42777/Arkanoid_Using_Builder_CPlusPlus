02e3be33554db05761b03a32472d3c27c531c778
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-05-18 13:25:28 +0100

    Background done, ball placed on background(moving and reflecting from left and top wall-rught wall not done yet

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,10 +8,37 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
+int x = -8;    //8 positions left
+int y = -8;; // 8 positions up
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::TimerBallTimer(TObject *Sender)
+{
+       ball->Left += x;
+       ball->Top += y;
+       //reflect from left wall
+
+       if(ball->Left - 5 <= background->Left)   // -5 pixels
+       {
+       x = -x;
+       }
+
+       if(ball->Top - 5 <= background->Top)
+       {
+       y = -y;
+       }
+       if(ball->Left - 5 <= background->Left)   // -5 pixels
+       {
+       x = -x;
+       }
+
+
+
+}
+//---------------------------------------------------------------------------
